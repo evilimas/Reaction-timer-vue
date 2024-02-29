ab8482a8f19b7a7adbf3604745e910de472812c1
evilimas <eg.vilimas@gmail.com>
2024-02-29 14:03:31 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <p>Want to check your reaction time?</p>
+  <button @click="start" :disabled="isPlaying">Press Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
+</template>
+
+<script>
+import Block from './components/Block.vue';
+import Results from './components/Results.vue';
+
+export default {
+  name: 'App',
+  components: { Block, Results },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 1000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #1d1d1d;
+  margin-top: 60px;
+}
+body {
+  background: #c4c4c4;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 9px 16px;
+  border-radius: 6px;
+  font-size: 20px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.4;
+  cursor: not-allowed;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,37 @@
+<template>
+  <h4>
+    Your Reaction Time is: <span> {{ score }} </span>ms
+  </h4>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Ninja Fingers';
+    } else if (this.score < 400) {
+      this.rank = 'Fast Reflexes';
+    } else {
+      this.rank = 'Pretty Slow...';
+    }
+  },
+};
+</script>
+
+<style>
+span {
+  color: #0faf87;
+}
+.rank {
+  font-size: 1.4em;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
